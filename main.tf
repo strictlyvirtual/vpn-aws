be2e0098e5e8e18d48148361feaf07b61d63aa19
Ruslan Saydulaev <ruslan@plutoview.com>
2022-08-31 19:32:14 +0300

    format

--- main.tf
+++ main.tf
@@ -21,43 +21,54 @@ resource "aws_vpn_connection" "this" {
   remote_ipv4_network_cidr                = var.remote_ipv4_network_cidr
   remote_ipv6_network_cidr                = var.remote_ipv6_network_cidr
   transport_transit_gateway_attachment_id = can(var.outside_ip_address_type == "PrivateIpv4") ? var.transport_transit_gateway_attachment_id : null
-  tunnel1_inside_cidr                     = var.tunnel1_inside_cidr
-  tunnel2_inside_cidr                     = var.tunnel2_inside_cidr
-  tunnel1_inside_ipv6_cidr                = try(var.transit_gateway_id, false) ? var.tunnel1_inside_ipv6_cidr : null
-  tunnel2_inside_ipv6_cidr                = try(var.transit_gateway_id, false) ? var.tunnel2_inside_ipv6_cidr : null
-  tunnel1_preshared_key                   = var.tunnel1_preshared_key
-  tunnel2_preshared_key                   = var.tunnel2_preshared_key
-  tunnel1_dpd_timeout_action              = var.tunnel1_dpd_timeout_action
-  tunnel2_dpd_timeout_action              = var.tunnel2_dpd_timeout_action
-  tunnel1_dpd_timeout_seconds             = var.tunnel1_dpd_timeout_seconds
-  tunnel2_dpd_timeout_seconds             = var.tunnel2_dpd_timeout_seconds
-  tunnel1_ike_versions                    = toset(var.tunnel1_ike_versions)
-  tunnel2_ike_versions                    = toset(var.tunnel2_ike_versions)
+
+  tunnel1_inside_cidr         = var.tunnel1_inside_cidr
+  tunnel1_inside_ipv6_cidr    = try(var.transit_gateway_id, false) ? var.tunnel1_inside_ipv6_cidr : null
+  tunnel1_preshared_key       = var.tunnel1_preshared_key
+  tunnel1_dpd_timeout_action  = var.tunnel1_dpd_timeout_action
+  tunnel1_dpd_timeout_seconds = var.tunnel1_dpd_timeout_seconds
+  tunnel1_ike_versions        = toset(var.tunnel1_ike_versions)
+
+  tunnel2_inside_cidr         = var.tunnel2_inside_cidr
+  tunnel2_inside_ipv6_cidr    = try(var.transit_gateway_id, false) ? var.tunnel2_inside_ipv6_cidr : null
+  tunnel2_preshared_key       = var.tunnel2_preshared_key
+  tunnel2_dpd_timeout_action  = var.tunnel2_dpd_timeout_action
+  tunnel2_dpd_timeout_seconds = var.tunnel2_dpd_timeout_seconds
+  tunnel2_ike_versions        = toset(var.tunnel2_ike_versions)
+
   // phase1
   tunnel1_phase1_dh_group_numbers      = var.tunnel1_phase1_dh_group_numbers
-  tunnel2_phase1_dh_group_numbers      = var.tunnel2_phase1_dh_group_numbers
   tunnel1_phase1_encryption_algorithms = var.tunnel1_phase1_encryption_algorithms
-  tunnel2_phase1_encryption_algorithms = var.tunnel2_phase1_encryption_algorithms
   tunnel1_phase1_integrity_algorithms  = var.tunnel1_phase1_integrity_algorithms
-  tunnel2_phase1_integrity_algorithms  = var.tunnel2_phase1_integrity_algorithms
   tunnel1_phase1_lifetime_seconds      = var.tunnel1_phase1_lifetime_seconds
+
+  tunnel2_phase1_dh_group_numbers      = var.tunnel2_phase1_dh_group_numbers
+  tunnel2_phase1_encryption_algorithms = var.tunnel2_phase1_encryption_algorithms
+  tunnel2_phase1_integrity_algorithms  = var.tunnel2_phase1_integrity_algorithms
   tunnel2_phase1_lifetime_seconds      = var.tunnel2_phase1_lifetime_seconds
+
   // phase2
   tunnel1_phase2_dh_group_numbers      = var.tunnel1_phase2_dh_group_numbers
-  tunnel2_phase2_dh_group_numbers      = var.tunnel2_phase2_dh_group_numbers
   tunnel1_phase2_encryption_algorithms = var.tunnel1_phase2_encryption_algorithms
-  tunnel2_phase2_encryption_algorithms = var.tunnel2_phase2_encryption_algorithms
   tunnel1_phase2_integrity_algorithms  = var.tunnel1_phase2_integrity_algorithms
-  tunnel2_phase2_integrity_algorithms  = var.tunnel2_phase2_integrity_algorithms
   tunnel1_phase2_lifetime_seconds      = var.tunnel1_phase2_lifetime_seconds
-  tunnel2_phase2_lifetime_seconds      = var.tunnel2_phase2_lifetime_seconds
   tunnel1_rekey_fuzz_percentage        = var.tunnel1_rekey_fuzz_percentage
-  tunnel2_rekey_fuzz_percentage        = var.tunnel2_rekey_fuzz_percentage
   tunnel1_rekey_margin_time_seconds    = var.tunnel1_rekey_margin_time_seconds
-  tunnel2_rekey_margin_time_seconds    = var.tunnel2_rekey_margin_time_seconds
   tunnel1_replay_window_size           = var.tunnel1_replay_window_size
-  tunnel2_replay_window_size           = var.tunnel2_replay_window_size
   tunnel1_startup_action               = var.tunnel1_startup_action
+
+  tunnel2_phase2_dh_group_numbers      = var.tunnel2_phase2_dh_group_numbers
+  tunnel2_phase2_encryption_algorithms = var.tunnel2_phase2_encryption_algorithms
+  tunnel2_phase2_integrity_algorithms  = var.tunnel2_phase2_integrity_algorithms
+  tunnel2_phase2_lifetime_seconds      = var.tunnel2_phase2_lifetime_seconds
+  tunnel2_rekey_fuzz_percentage        = var.tunnel2_rekey_fuzz_percentage
+  tunnel2_rekey_margin_time_seconds    = var.tunnel2_rekey_margin_time_seconds
+  tunnel2_replay_window_size           = var.tunnel2_replay_window_size
   tunnel2_startup_action               = var.tunnel2_startup_action
-  tags                                 = merge(var.tags, { Name = "vpn-${replace(var.resource_name, "_", "-")}" })
+
+  tags = merge(var.tags, { Name = "vpn-${replace(var.resource_name, "_", "-")}" })
+
+  depends_on = [
+    aws_customer_gateway.this
+  ]
 }

344ad6df2af0ca25fa8ef734ccc0f3a9002a9206
Ruslan Saydulaev <ruslan@plutoview.com>
2022-10-02 00:57:26 +0300

    up versions

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.2.2"
+  required_version = ">= 1.3.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.26.0"
+      version = ">= 4.33.0"
     }
 
     random = {
